e7075a0bbd93645729e4d18e37edb41fc36236ee
Nexums07 <nexums.startup@gmail.com>
2024-06-26 20:14:20 -0300

    Modularização do código

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+  return (
+    <footer className="info">
+      <p>Double-click to edit a todo</p>
+      <p>Created by the TodoMVC Team</p>
+      <p>
+        Part of
+        <span> </span> 
+        <a href="http://todomvc.com">TodoMVC</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
