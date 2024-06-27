9473838ba33fa3bf30510b85085979df8e46a895
matheus-dsa <matheusdasilvaassis4@gmail.com>
2024-06-27 01:56:45 -0300

    implementação de testes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="info">
       <p>Double-click to edit a todo</p>
-      <p>Created by the TodoMVC Team</p>
+      <p>Created by Matheus Assis</p>
       <p>
         Part of
         <span> </span> 
